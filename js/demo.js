bb9b1a654ac27cee8a2d809c320447c80e0dd866
0Xbug1971 <72446101+0Xbug1971@users.noreply.github.com>
2023-10-01 02:22:11 +0600

    Update demo.js

--- js/demo.js
+++ js/demo.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
 }, false);
 
 
-/*
+
 // jQuery plugin example:
 $(document).ready(function() {
   $('#particles').particleground({
@@ -25,4 +25,45 @@ $(document).ready(function() {
     'margin-top': -($('.intro').height() / 2)
   });
 });
-*/
+
+
+function init() {
+      if (!canvasSupport) { return; }
+
+      //Create canvas
+      canvas = document.createElement('canvas');
+      canvas.className = 'pg-canvas';
+      canvas.style.display = 'block';
+      element.insertBefore(canvas, element.firstChild);
+      ctx = canvas.getContext('2d');
+      styleCanvas();
+
+      // Create particles
+      var numParticles = Math.round((canvas.width * canvas.height) / options.density);
+      for (var i = 0; i < numParticles; i++) {
+        var p = new Particle();
+        p.setStackPos(i);
+        particles.push(p);
+      };
+
+      window.addEventListener('resize', function() {
+        resizeHandler();
+      }, false);
+
+      document.addEventListener('mousemove', function(e) {
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+      }, false);
+
+      if (orientationSupport && !desktop) {
+        window.addEventListener('deviceorientation', function () {
+          // Contrain tilt range to [-30,30]
+          tiltY = Math.min(Math.max(-event.beta, -30), 30);
+          tiltX = Math.min(Math.max(-event.gamma, -30), 30);
+        }, true);
+      }
+
+      draw();
+      hook('onInit');
+    }
+
